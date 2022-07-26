05578f28b9ac85e331b77d6c4b1fb42878803dd8
Jar'Zeno <jarzenojarrett@gmail.com>
2022-07-26 08:42:03 -0700

    Finish door-policy exercise

--- javascript/poetry-club-door-policy/door-policy.js
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -19,6 +19,8 @@
 //
 // Good luck with that door policy!
 
+import { workerData } from "worker_threads";
+
 /**
  * Respond with the correct character, given the line of the
  * poem, if this were said at the front door.
@@ -26,9 +28,7 @@
  * @param {string} line
  * @returns {string}
  */
-export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
-}
+export let frontDoorResponse = line => line[0];
 
 /**
  * Format the password for the front-door, given the response
@@ -37,9 +37,7 @@ export function frontDoorResponse(line) {
  * @param {string} word the letters you responded with before
  * @returns {string} the front door password
  */
-export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
-}
+export let frontDoorPassword = word => word[0].toUpperCase() + word.slice(1).toLowerCase();
 
 /**
  * Respond with the correct character, given the line of the
@@ -48,9 +46,7 @@ export function frontDoorPassword(word) {
  * @param {string} line
  * @returns {string}
  */
-export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
-}
+export let backDoorResponse = line => line.trim()[line.trim().length - 1];
 
 /**
  * Format the password for the back door, given the response
@@ -59,6 +55,4 @@ export function backDoorResponse(line) {
  * @param {string} word the letters you responded with before
  * @returns {string} the back door password
  */
-export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
-}
+export let backDoorPassword = word => word[0].toUpperCase() + word.slice(1).toLowerCase() + ', please'
